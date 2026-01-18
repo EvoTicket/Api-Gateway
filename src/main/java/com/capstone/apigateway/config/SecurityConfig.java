85d038875c243e996ff082f967e6f8a5d61bd8d2
KuaBK <tuanphonglqd@gmail.com>
2026-01-18 21:58:41 +0700

    remove redis

--- src/main/java/com/capstone/apigateway/config/SecurityConfig.java
+++ src/main/java/com/capstone/apigateway/config/SecurityConfig.java
@@ -1,15 +1,9 @@
 package com.capstone.apigateway.config;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
-import io.jsonwebtoken.Claims;
 
 @Configuration
 public class SecurityConfig {
@@ -27,34 +21,5 @@ public class SecurityConfig {
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable);
 
         return http.build();
-    }
-
-    @Bean
-    public KeyResolver jwtUserKeyResolver() {
-        return exchange -> {
-            String auth = exchange.getRequest()
-                    .getHeaders()
-                    .getFirst("Authorization");
-
-            if (auth == null || !auth.startsWith("Bearer ")) {
-                return Mono.just("anonymous");
-            }
-            String token = auth.substring(7);
-
-            byte[] keyBytes = Decoders.BASE64.decode("");
-
-            Claims claims = Jwts
-                    .parserBuilder()
-                    .setSigningKey(Keys.hmacShaKeyFor(keyBytes))
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-
-            Long userId = claims.get("userId", Long.class);
-
-            System.out.println("RATE LIMIT USER = " + userId);
-
-            return Mono.just(String.valueOf(userId));
-        };
     }
 }
